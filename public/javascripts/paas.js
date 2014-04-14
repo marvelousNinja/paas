ca976957ff78cfa1dab5f6d01f4bc5d20715fae7
Alexander Sologub <alexsologoub@gmail.com>
2014-04-14 22:39:38 +0300

    Basic workflow: You can now do what you want to do

--- public/javascripts/paas.js
+++ public/javascripts/paas.js
@@ -1,7 +1,15 @@
 var paas = angular.module('PaaS', ['ui.router']);
 
-paas.controller('PController', function($scope) {
+paas.controller('PController', function($scope, $state, $timeout) {
+  $scope.sendBestRegards = function(target) {
+    $state.transitionTo('targeting');
 
+    $timeout(function() {
+      $state.transitionTo('completed', {
+        target: target
+      });
+    }, 5000);
+  }
 });
 
 paas.config(function($stateProvider) {
@@ -9,6 +17,16 @@ paas.config(function($stateProvider) {
     .state('form', {
       url: '',
       templateUrl: '/templates/form.html',
+      controller: 'PController'
+    })
+    .state('targeting', {
+      url: 'targeting',
+      templateUrl: '/templates/targeting.html',
+      controller: 'PController'
+    })
+    .state('completed', {
+      url: 'completed',
+      templateUrl: '/templates/completed.html',
       controller: 'PController'
     })
 });
